26c241c2369b666b4a7ce6fe57417b192a8541fc
CodeAGenie <nwibeg@gmail.com>
2023-06-26 13:33:52 +0100

    update

--- newsletter-sign-up-with-success-message-main/app.js
+++ newsletter-sign-up-with-success-message-main/app.js
@@ -1,18 +1,51 @@
 const container = document.getElementById("container")
-const Main = document.getElementById("main")
-const Signup = document.getElementById("signupForm")
-const Useremail = document.getElementById("Email")
-const  Submit = document.getElementById("submitForm")
-const  Error = document.getElementById("errorText")
-const  Banner = document.getElementById("banner")
-const  Successful = document.getElementById("success")
-const  Dismiss = document.getElementById("dismiss")
+const main = document.getElementById("main")
+const signup = document.getElementById("signupForm")
+const userEmail = document.getElementById("email")
+const  submit = document.getElementById("submitForm")
+const  error = document.getElementById("errorText")
+const  banner = document.getElementById("banner")
+const  successful = document.getElementById("success")
+const  dismiss = document.getElementById("dismiss")
 
 function forSuccess(){
-    Successful.classList.add("active")
+    success.classList.add("active")
     container.classList.add("success")
+    banner.style.display = "none"
+    main.style.display = "none"
 }
 
+function validateEmail(){
+    const emailRegex =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email)
+}
+
+Submit.addEventListener("click", (e) => {
+     e.preventDefault(e)
+     let emailInput = document.getElementById("email")
+      userEmail = emailInput.value.trim()
+      
+      let errorText = document.getElementById("errorText");
+      if(validateEmail(email)){
+        forSuccess()
+        userEmail.innerText = email
+        email.value =""
+
+        errorText.classList.remove("active")
+        email.classList.remove("active")
+      }else{
+        errorText.classList.add("active")
+        email.classList.add("active")
+      }
+
+})
+
+dismiss.addEventListener("click"  () => {
+    banner.style.display = "block"
+    main.style.display ="block"
+    success.classList.remove("active")
+
+})
 
 
-
+ 
