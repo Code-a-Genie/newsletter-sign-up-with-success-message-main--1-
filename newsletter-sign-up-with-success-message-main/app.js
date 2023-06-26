cb62023b6ae1c0d124e75d4bc3bb9ac9ee86eb01
CodeAGenie <nwibeg@gmail.com>
2023-06-26 11:49:29 +0100

    Update

---
+++ newsletter-sign-up-with-success-message-main/app.js
@@ -0,0 +1,18 @@
+const container = document.getElementById("container")
+const Main = document.getElementById("main")
+const Signup = document.getElementById("signupForm")
+const Useremail = document.getElementById("Email")
+const  Submit = document.getElementById("submitForm")
+const  Error = document.getElementById("errorText")
+const  Banner = document.getElementById("banner")
+const  Successful = document.getElementById("success")
+const  Dismiss = document.getElementById("dismiss")
+
+function forSuccess(){
+    Successful.classList.add("active")
+    container.classList.add("success")
+}
+
+
+
+
